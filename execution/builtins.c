eb44fd5105fb7bb1295b833fd2af51d8ef9f3586
filly7721 <hmooodshalaab@gmail.com>
2024-11-09 21:15:31 +0400

    added ft_export

--- execution/builtins.c
+++ execution/builtins.c
@@ -66,6 +66,105 @@ int	ft_unset(t_shell *shell)
 	return (0);
 }
 
+bool	validate_export(char *str)
+{
+	if (!ft_isalpha(*str) && *str != '_')
+		return (false);
+	while (*str != '\0' && *str != '=')
+	{
+		if (!ft_isalnum(*str) && *str != '_')
+			return (false);
+		str++;
+	}
+	return (true);
+}
+
+void	print_export(t_list *list, int fd)
+{
+	char	*end;
+	t_list	*curr;
+
+	curr = list;
+	while (curr)
+	{
+		end = ft_strchr(curr->content, '=') + 1;
+		ft_putstr_fd("declare -x ", fd);
+		write(fd, curr->content, end - (char *)curr->content);
+		ft_putchar_fd('"', fd);
+		ft_putstr_fd(end, fd);
+		ft_putstr_fd("\"\n", fd);
+		curr = curr->next;
+	}
+}
+
+int	sort_print(t_list *list, int fd)
+{
+	int		count;
+	char	*temp;
+	t_list	*curr;
+
+	if (!list)
+		return (ft_putstr_fd("An error has occurred\n", 2), 1);
+	count = ft_lstsize(list);
+	while (count)
+	{
+		curr = list;
+		while (curr->next)
+		{
+			if (ft_strncmp(curr->content, curr->next->content, -1) > 0)
+			{
+				temp = curr->content;
+				curr->content = curr->next->content;
+				curr->next->content = temp;
+			}
+			curr = curr->next;
+		}
+		count--;
+	}
+	print_export(list, fd);
+	return (ft_lstclear(&list, free), 0);
+}
+
+bool	add_env(t_list **list, char *str)
+{
+	char	*copy;
+	t_list	*temp;
+
+	if (ft_strchr(str, '=') != NULL)
+		copy = ft_strdup(str);
+	else
+		copy = ft_strjoin(str, "=");
+	if (!copy)
+		return (ft_putstr_fd("An error has occured\n", 2), 1);
+	temp = ft_lstnew(copy);
+	if (!temp)
+		return (ft_putstr_fd("An error has occured\n", 2), free(copy), 1);
+	ft_lstadd_front(list, temp);
+	return (true);
+}
+
+int	ft_export(t_shell *shell)
+{
+	int		i;
+	int		fd;
+
+	fd = shell->context->output;
+	if (fd == -1)
+		fd = 1;
+	i = 1;
+	while (shell->context->args[i])
+	{
+		if (!validate_export(shell->context->args[i]))
+			return (ft_putstr_fd("Invalid variable name\n", 2), 1);
+		if (!add_env(&shell->env, shell->context->args[i]))
+			return (ft_putstr_fd("An error has occured\n", 2), 1);
+		i++;
+	}
+	if (shell->context->args[1] == NULL)
+		return (sort_print(ft_lstmap(shell->env, (void *(*)(void *))ft_strdup, free), fd));
+	return (0);
+}
+
 int	execute_builtin(t_shell *shell, char **env)
 {
 	int	status;
@@ -77,6 +176,8 @@ int	execute_builtin(t_shell *shell, char **env)
 		status = ft_pwd(shell->context);
 	else if (ft_strncmp(shell->context->cmd, "unset", -1) == 0)
 		status = ft_unset(shell);
+	else if (ft_strncmp(shell->context->cmd, "export", -1) == 0)
+		status = ft_export(shell);
 	else
 	{
 		ft_putstr_fd("unhandled builtin", 2);
@@ -97,6 +198,8 @@ bool	is_builtin(char *str)
 	else if (ft_strncmp(str, "pwd", -1) == 0)
 		return (true);
 	else if (ft_strncmp(str, "unset", -1) == 0)
+		return (true);
+	else if (ft_strncmp(str, "export", -1) == 0)
 		return (true);
 	return (false);
 }
