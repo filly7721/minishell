482fa1a2d4e48713f027116264f400766275e05c
filly7721 <hmooodshalaab@gmail.com>
2024-11-24 23:18:39 +0400

    added variable name checking in unset

--- execution/builtins.c
+++ execution/builtins.c
@@ -217,11 +217,19 @@ int	ft_pwd(t_shell *shell, char **env, t_context *context)
 int	ft_unset(t_shell *shell)
 {
 	int		i;
+	int		j;
 	char	*name;
 
 	i = 1;
 	while (shell->context->args[i])
 	{
+		j = 0;
+		if (!ft_isalpha(shell->context->args[i][j]) && shell->context->args[i][j] != '_')
+			return (ft_putstr_fd("Invalid variable name\n", 2), 1);
+		while (ft_isalnum(shell->context->args[i][j]) || shell->context->args[i][j] == '_')
+			j++;
+		if (shell->context->args[i][j] != '\0')
+			return (ft_putstr_fd("Invalid variable name\n", 2), 1);
 		name = ft_strjoin(shell->context->args[i], "=");
 		if (!name)
 			return (ft_putstr_fd("An error has occurred\n", 2), 1);
