0d7e293317377c6721e80e6985c6b3a6d14d9947
sameeha101 <sameehanaaz7@gmail.com>
2024-11-25 22:34:09 +0400

    properly echo echo echo

--- execution/builtins/ft_echo.c
+++ execution/builtins/ft_echo.c
@@ -6,27 +6,35 @@
 /*   By: ssiddiqu <ssiddiqu@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:01:51 by ssiddiqu          #+#    #+#             */
-/*   Updated: 2024/11/25 18:02:27 by ssiddiqu         ###   ########.fr       */
+/*   Updated: 2024/11/25 22:34:00 by ssiddiqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+bool	check_flag(char *str, char c)
+{
+	int	i;
+
+	if (str[0] != '-')
+		return (false);
+	i = 1;
+	while(str[i] == c)
+		i++;
+	return (str[i] == '\0');
+}
+
 int	ft_echo(t_context *context)
 {
 	char	**strs;
-	char	*flags;
 	int		fd;
-	int		i;
 
 	fd = context->output;
 	if (fd == -1)
 		fd = 1;
-	i = 1;
-	flags = get_arg_flags(context->args, &i);
-	if (!flags)
-		return (ft_putstr_fd("An error has occurred\n", 2), 1);
-	strs = context->args + i;
+	strs = context->args + 1;
+	if (context->args[1] && check_flag(context->args[1], 'n'))
+		strs++;
 	if (*strs)
 		ft_putstr_fd(*strs++, fd);
 	while (*strs)
@@ -34,8 +42,8 @@ int	ft_echo(t_context *context)
 		ft_putchar_fd(' ', fd);
 		ft_putstr_fd(*strs++, fd);
 	}
-	if (ft_strchr(flags, 'n') == NULL)
+	if (!context->args[1] || !check_flag(context->args[1], 'n'))
 		ft_putchar_fd('\n', fd);
-	free(flags);
 	return (0);
 }
+
